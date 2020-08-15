1da4da2ca1521c2c3d61db0425cc3e305b8e59fc
Matthew Whited <matt@whited.us>
2020-08-15 08:08:01 -0400

    Changes Detected: Sat 15 Aug 2020 08:08:01 AM EDT

--- backup.sh
+++ backup.sh
@@ -10,8 +10,7 @@ apt list --installed > apt.installed
 #Ensure everything belongs to this account
 sudo chown pi:pi . -R
 
-#git add -A duet3
-## git reset duet3/dsf_sd/www
-#git commit -m "Changes from DSF Detected: $(date)"
-#git push
+git add -A 
+git commit -m "Changes Detected: $(date)"
+git push
 
